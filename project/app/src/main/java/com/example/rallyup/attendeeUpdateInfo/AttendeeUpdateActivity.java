f641e0099ff5955a690f31552da2e3e82cdf71a5
FifteenPeruvianHuevos <wuchihhung0905@gmail.com>
2024-03-05 13:46:53 -0700

    Update AttendeeUpdateActivity.java
    Just modified the confirm button so that it brings us back to the previous activity (in this case: com.example.rallyup.MainActivity.class)

--- project/app/src/main/java/com/example/rallyup/attendeeUpdateInfo/AttendeeUpdateActivity.java
+++ project/app/src/main/java/com/example/rallyup/attendeeUpdateInfo/AttendeeUpdateActivity.java
@@ -1,5 +1,6 @@
 package com.example.rallyup.attendeeUpdateInfo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,7 @@ import android.widget.ImageView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.rallyup.MainActivity;
 import com.example.rallyup.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -89,6 +91,10 @@ public class AttendeeUpdateActivity extends AppCompatActivity {
                 // "user.phoneNumber" = editPhoneNumber.getText().toString();
                 // "user.geolocationOn" = geolocationCheck.isChecked();
 
+                // Since we clicked on confirm, it brings us back to the screen that was there before
+                // In this case, we'll put MainActivity.class as the placeholder
+                Intent intent = new Intent(AttendeeUpdateActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
