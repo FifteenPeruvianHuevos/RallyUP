4c735685c02e4faf5deae9b94b578b6c5b388793
Isla Samantha <104488419+ohitssla@users.noreply.github.com>
2024-03-06 15:23:28 -0700

    adding registered event ui

---
+++ project/app/src/main/java/com/example/rallyup/uiReference/AttendeeRegisteredEvent.java
@@ -0,0 +1,16 @@
+package com.example.rallyup.uiReference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rallyup.R;
+
+public class AttendeeRegisteredEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendee_registered_event);
+    }
+}
