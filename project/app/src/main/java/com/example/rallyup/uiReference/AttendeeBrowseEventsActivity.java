e70f538951660a820869dbc3415df5ee0f6569e6
kchrisalis <kayecryz@ualberta.ca>
2024-03-07 12:46:29 -0700

    adding more xml stuff

--- project/app/src/main/java/com/example/rallyup/uiReference/AttendeeBrowseEventsActivity.java
+++ project/app/src/main/java/com/example/rallyup/uiReference/AttendeeBrowseEventsActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.example.rallyup.R;
 
-public class AttendeeBrowseEventsActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class AttendeeBrowseEventsActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<Integer> arrayList = new ArrayList<>();
 
     ImageButton attBrowseEventsBackBtn;
 
@@ -20,6 +25,17 @@ public class AttendeeBrowseEventsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.attendee_browse_events);
 
+        listView = findViewById(R.id.att_browse_events_list);
+
+        arrayList.add(R.drawable.poster1);
+        arrayList.add(R.drawable.poster2);
+        arrayList.add(R.drawable.poster1);
+        arrayList.add(R.drawable.poster2);
+        arrayList.add(R.drawable.poster1);
+        arrayList.add(R.drawable.poster2);
+
+        ListAdapter listAdapter = new ListAdapter(AttendeeBrowseEventsActivity.this, arrayList);
+        listView.setAdapter(listAdapter);
 
         attBrowseEventsBackBtn = findViewById(R.id.browse_events_back_button);
 
@@ -30,6 +46,16 @@ public class AttendeeBrowseEventsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        // browse_events_back_button
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Integer poster = (Integer) adapterView.getItemAtPosition(i);
+
+                Intent appInfo = new Intent(getBaseContext(), AttendeeEventDetails.class);
+//                appInfo.putExtra("poster", poster);
+                startActivity(appInfo);
+            }
+        });
     }
 }
--- project/app/src/main/java/com/example/rallyup/uiReference/AttendeeEventDetails.java
+++ project/app/src/main/java/com/example/rallyup/uiReference/AttendeeEventDetails.java
@@ -2,15 +2,34 @@ package com.example.rallyup.uiReference;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.example.rallyup.R;
+import com.example.rallyup.attendeeList.AttListArrayAdapter;
+import com.example.rallyup.attendeeList.AttendeeStatsClass;
+
+import java.util.ArrayList;
 
 public class AttendeeEventDetails extends AppCompatActivity {
 
+    ImageButton attEventBackButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendee_event_details);
+
+        attEventBackButton = findViewById(R.id.attendee_event_back_button);
+
+        attEventBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), AttendeeBrowseEventsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
